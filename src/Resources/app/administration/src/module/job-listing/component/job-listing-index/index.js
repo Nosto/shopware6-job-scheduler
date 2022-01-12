01b998350f03adfed107e39e16da387b0c24b297
Sergiy <41483861+SergiyStasyuk@users.noreply.github.com>
2022-01-12 19:15:37 +0200

    add template and js for job list grid in BO [https://www.wrike.com/open.htm?id=824452285]

---
+++ src/Resources/app/administration/src/module/job-listing/component/job-listing-index/index.js
@@ -0,0 +1,86 @@
+import template from './job-listing-index.html.twig';
+import './job-listing-index.scss';
+
+const {Component} = Shopware;
+const {Criteria} = Shopware.Data;
+
+Component.register('job-listing-index', {
+    template,
+
+    inject: [
+        'repositoryFactory'
+    ],
+
+    computed: {
+        columns() {
+            return this.getColumns();
+        }
+    },
+
+    created() {
+        this.createdComponent();
+    },
+
+    data: function () {
+        console.log(result);
+        return {
+            repository: null,
+            result: null
+        }
+    },
+
+    methods: {
+        createdComponent() {
+            this.repository = this.repositoryFactory.create('od_scheduler_job');
+
+            const criteria = new Criteria();
+            criteria.addFilter(Criteria.equalsAny('type','test'));
+
+            this.repository.search(criteria,Shopware.Context.api).then(result => {
+                this.result = result;
+            });
+
+        },
+
+        getColumns() {
+            return [
+                {
+                    property: 'id',
+                    label: this.$tc('job-listing.page.listing.grid.column.id'),
+                    allowResize: true,
+                    primary: true
+                },
+                {
+                    property: 'status',
+                    label: this.$tc('job-listing.page.listing.grid.column.status'),
+                    allowResize: true
+                },
+                {
+                    property: 'type',
+                    label: this.$tc('job-listing.page.listing.grid.column.type'),
+                    allowResize: true
+                },
+                {
+                    property: 'name',
+                    label: this.$tc('job-listing.page.listing.grid.column.name'),
+                    allowResize: true
+                },
+                {
+                    property: 'message',
+                    label: this.$tc('job-listing.page.listing.grid.column.message'),
+                    allowResize: true
+                },
+                {
+                    property: 'startedAt',
+                    label: this.$tc('job-listing.page.listing.grid.column.started-at'),
+                    allowResize: true
+                },
+                {
+                    property: 'finishedAt',
+                    label: this.$tc('job-listing.page.listing.grid.column.finished-at'),
+                    allowResize: true
+                },
+            ];
+        },
+    }
+});
---
+++ src/Resources/app/administration/src/module/job-listing/index.js
@@ -0,0 +1,47 @@
+import './component/job-listing-index';
+
+const {Module} = Shopware;
+
+Module.register('job-listing', {
+    type: 'plugin',
+    title: 'job-listing.general.title',
+    description: 'job-listing.general.description',
+    color: '#F88962',
+    icon: 'default-avatar-multiple',
+
+    routes: {
+        index: {
+            component: 'job-listing-index',
+            path: 'index'
+        }
+    },
+
+    // navigation: [{
+    //     id: 'job-listing',
+    //     label: 'job-listing.general.title',
+    //     color: '#F88962',
+    //     icon: 'default-avatar-multiple',
+    //     parent: 'sw-customer',
+    //     position: 40
+    // }],
+
+    navigation: [
+        {
+            id: 'job-listing',
+            label: 'job-listing.menu.title',
+            color: '#F88962',
+            icon: 'default-avatar-multiple',
+            parent: 'sw-marketing',
+            position: 100
+        },
+        {
+            label: 'job-listing.general.title',
+            color: '#77ff3d',
+            icon: 'small-default-stack-line2',
+            path: 'job.listing.index',
+            parent: 'job-listing',
+            position: 100
+        },
+    ],
+
+})
