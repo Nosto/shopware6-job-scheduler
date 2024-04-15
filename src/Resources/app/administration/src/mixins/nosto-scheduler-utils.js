e9b174e543bb9ce33129d24d5a514d188a6a64dc
Tobias Graml <tobias.graml@nosto.com>
2024-04-15 10:13:54 +0200

    Fix linting

--- src/Resources/app/administration/src/mixins/nosto-scheduler-utils.js
+++ src/Resources/app/administration/src/mixins/nosto-scheduler-utils.js
@@ -3,7 +3,7 @@ const { Mixin, Utils } = Shopware;
 Mixin.register('nosto-scheduler-utils', {
     methods: {
         formatDate(date) {
-            return Utils.format.date(date)
-        }
-    }
+            return Utils.format.date(date);
+        },
+    },
 });
