ebb4bc496bffa3066844b8da547234e96ecc70dd
Ugljesa  Zivaljevic <ugljesa.zivaljevic@soprex.com>
2025-06-15 18:48:58 +0200

    Sub jobs filtering

--- src/Resources/app/administration/src/component/nosto-job-sub-jobs/index.js
+++ src/Resources/app/administration/src/component/nosto-job-sub-jobs/index.js
@@ -22,16 +22,22 @@ Component.register('nosto-job-sub-jobs', {
     props: {
         jobId: {
             type: String,
-            required: false,
-            default: null,
+            required: true
         },
     },
 
     data() {
         return {
-            subJobs: null,
+            subJobs: [],
             showMessagesModal: false,
             currentJobMessages: null,
+            statusFilter: 'all',
+            isLoading: true,
+            sortBy: 'createdAt',
+            sortDirection: 'DESC',
+            total: 0,
+            page: 1,
+            limit: 25,
         };
     },
 
@@ -39,33 +45,46 @@ Component.register('nosto-job-sub-jobs', {
         jobRepository() {
             return this.repositoryFactory.create('nosto_scheduler_job');
         },
+        filteredSubJobs() {
+            if (!this.subJobs || !Array.isArray(this.subJobs)) {
+                return [];
+            }
+
+            let jobs = this.subJobs;
+
+            if (this.statusFilter !== 'all') {
+                jobs = jobs.filter(job => job.status === this.statusFilter);
+            }
+
+            return jobs;
+        },
 
         jobChildrenColumns() {
             return [
                 {
                     property: 'name',
                     dataIndex: 'name',
-                    label: this.$tc('job-listing.page.listing.grid.column.name'),
-                    allowResize: false,
-                    inlineEdit: true,
-                    width: '200px',
+                    label: 'Name',
+                    allowResize: true,
+                    primary: true,
+                    sortable: true,
+                    width: '250px'
                 },
                 {
                     property: 'status',
                     dataIndex: 'status',
-                    label: this.$tc('job-listing.page.listing.grid.column.status'),
-                    allowResize: false,
-                    inlineEdit: true,
-                    width: '100px',
+                    label: 'Status',
+                    allowResize: true,
+                    sortable: true,
+                    width: '150px'
                 },
                 {
                     property: 'startedAt',
                     dataIndex: 'startedAt',
-                    label: this.$tc('job-listing.page.listing.grid.column.started-at'),
-                    allowResize: false,
-                    inlineEdit: true,
-                    width: '150px',
+                    label: 'Started at',
+                    allowResize: true,
                     sortable: true,
+                    width: '200px'
                 },
                 {
                     property: 'finishedAt',
@@ -97,20 +116,50 @@ Component.register('nosto-job-sub-jobs', {
     },
 
     created() {
-        this.initModalData();
+        this.loadSubJobs();
     },
 
     methods: {
-        initModalData() {
-            const criteria = new Criteria();
-            criteria.addFilter(Criteria.equals('parentId', this.jobId));
-            criteria.addSorting(Criteria.sort('createdAt', 'DESC', false));
-            criteria.addAssociation('messages');
-            this.jobRepository.search(criteria, Shopware.Context.api).then(jobItems => {
-                this.subJobs = JobHelper.sortMessages(jobItems);
-            });
+        async loadSubJobs() {
+            if (!this.jobId) {
+                return;
+            }
+
+            this.isLoading = true;
+
+            try {
+                const criteria = new Criteria(this.page, this.limit);
+                criteria.addFilter(Criteria.equals('parentId', this.jobId));
+                criteria.addSorting(Criteria.sort(this.sortBy, this.sortDirection));
+                criteria.addAssociation('messages');
+
+                const result = await this.jobRepository.search(criteria, Shopware.Context.api);
+
+                this.total = result.total;
+                this.subJobs = result;
+
+                console.log('Loaded sub jobs:', this.subJobs); // Debug log
+            } catch (error) {
+                console.error('Error loading sub jobs:', error);
+                this.createNotificationError({
+                    message: this.$tc('job-listing.errors.load-sub-jobs-error'),
+                });
+            } finally {
+                this.isLoading = false;
+            }
         },
 
+        onSortColumn(column) {
+            this.sortBy = column.dataIndex;
+            this.sortDirection = this.sortDirection === 'ASC' ? 'DESC' : 'ASC';
+            this.loadSubJobs();
+        },
+
+        onPageChange({ page, limit }) {
+            this.page = page;
+            this.limit = limit;
+            this.loadSubJobs();
+        },
         rescheduleJob(jobId) {
             this.NostoRescheduleService.rescheduleJob(jobId).then(() => {
                 this.createNotificationSuccess({
@@ -129,10 +178,27 @@ Component.register('nosto-job-sub-jobs', {
             this.showMessagesModal = true;
         },
 
-        getMessagesCount(job, type) {
-            return job.messages.filter((item) => {
-                return item.type === `${type}-message`;
+        getMessagesCount(item, type) {
+            if (!item.messages) {
+                return 0;
+            }
+            return item.messages.filter(message => {
+                switch(type) {
+                    case 'info':
+                        return message.type === 'info-message';
+                    case 'warning':
+                        return message.type === 'warning-message';
+                    case 'error':
+                        return message.type === 'error-message';
+                    default:
+                        return false;
+                }
             }).length;
+        },
+
+        showMessages(item) {
+            this.currentJobMessages = item.messages;
+            this.showMessagesModal = true;
         },
     },
 });
