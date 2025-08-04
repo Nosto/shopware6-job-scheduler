af93dd33503af6cde2eec5c42c88056be7303f8f
Lisa <47558295+lisafindo@users.noreply.github.com>
2025-08-04 15:22:40 +0200

    adapt composer file

--- ecs.php
+++ ecs.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
 use PhpCsFixer\Fixer\Operator\NotOperatorWithSuccessorSpaceFixer;
+use SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff;
+use SlevomatCodingStandard\Sniffs\TypeHints\ReturnTypeHintSniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
@@ -13,7 +14,15 @@ return function (ECSConfig $ecsConfig): void {
     ]);
 
     $ecsConfig->skip([
-        NotOperatorWithSuccessorSpaceFixer::class
+        NotOperatorWithSuccessorSpaceFixer::class,
+    ]);
+
+    $ecsConfig->rulesWithConfiguration([
+        DeclareStrictTypesSniff::class => ['spacesCountAroundEqualsSign' => false],
+    ]);
+
+    $ecsConfig->rules([
+        ReturnTypeHintSniff::class,
     ]);
 
     $ecsConfig->sets([
