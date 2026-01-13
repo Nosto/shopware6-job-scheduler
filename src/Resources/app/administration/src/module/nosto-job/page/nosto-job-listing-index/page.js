ad4018175e7775de78d9e9f933133be1a09af9cc
Parisa Rahimi <parisa.rahimi@nosto.com>
2026-01-13 08:24:49 +0200

    1: Remove redundant import

--- src/Resources/app/administration/src/module/nosto-job/page/nosto-job-listing-index/page.js
+++ src/Resources/app/administration/src/module/nosto-job/page/nosto-job-listing-index/page.js
@@ -6,7 +6,6 @@ import template from './nosto-job-listing-index.html.twig';
 import JobHelper from '../../../../util/job.helper';
 import './nosto-job-listing-index.scss';
 
-const { Mixin } = Shopware;
 const { Criteria } = Shopware.Data;
 
 /** @private */
