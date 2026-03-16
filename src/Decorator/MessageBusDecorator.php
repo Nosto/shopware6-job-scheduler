76165870450b4ad7bea5dba7fd82f6afb19d4d70
Parisa Rahimi <parisa.rahimi@nosto.com>
2026-03-16 10:05:11 +0200

    Add child job tracking fields to job creation

--- src/Decorator/MessageBusDecorator.php
+++ src/Decorator/MessageBusDecorator.php
@@ -44,6 +44,8 @@ readonly class MessageBusDecorator implements MessageBusInterface
             'status' => JobEntity::TYPE_PENDING,
             'type' => $jobMessage->getHandlerCode(),
             'message' => $serializedEnvelope['body'] ?? null,
+            'expectedChildCount' => 0,
+            'childGenerationCompleted' => false,
         ];
 
         if ($jobMessage instanceof ParentAwareMessageInterface) {
