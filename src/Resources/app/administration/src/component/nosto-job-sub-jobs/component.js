e9adffb56304d705d7831aec0c33a47f3bebc09b
vladimir.chagorovski <vladimir.chagorovski@nosto.com>
2026-03-17 13:04:48 +0200

    ADS-5332 refactoring status rendering

--- src/Resources/app/administration/src/component/nosto-job-sub-jobs/component.js
+++ src/Resources/app/administration/src/component/nosto-job-sub-jobs/component.js
@@ -3,7 +3,6 @@
  */
 
 import template from './nosto-job-sub-jobs.html.twig';
-import { getJobStatusLabel, getJobStatusTone, isJobRunningStatus } from '../../util/job-status.helper';
 import fetchJobMessages from '../../util/job-messages.helper';
 import './nosto-job-sub-jobs.scss';
 
@@ -188,18 +187,6 @@ export default {
             }).catch(() => {
                 this.currentJobMessages = [];
             });
-        },
-
-        getStatusTone(status) {
-            return getJobStatusTone(status);
-        },
-
-        getStatusLabel(status) {
-            return getJobStatusLabel(status, (key) => this.$tc(key));
-        },
-
-        isRunningStatus(status) {
-            return isJobRunningStatus(status);
         },
     },
 };
